e2153f8606ff6cfdf133b1544a86c8529cb1d0a1
Yuvadep v <yuvadep.codes@gmail.com>
2025-10-14 23:42:02 +0530

    madlibs game-fill in the blank game

--- day2.py
+++ day2.py
@@ -2,4 +2,22 @@
 l=float(input("Enter The Length Of The Rectangle: "))
 b=float(input("Enter the Bridth Of The Rectangle: "))
 area=l*b
-print(f"the are of the rectangle is {area}")
+print(f"the area of the rectangle is {area}cm")
+
+#exercise 2 shopping cart
+item=input("what item would u like to buy ?")
+price=float(input("whats the price of it  "))
+quantity=float(input("how many would u like to have ? "))
+total=price*quantity
+print(f"your items are {item} for {price}*{quantity}")
+print(f"your total come to {total}")
+
+#exercise 3 madlibs game
+place=input("enter a place which u like ")
+person=input("enter the name of Your Friend ")
+activity=input("Enter A Activity You Like The Most ")
+place2=input("enter the place ")
+print(f"Today i went to {place}")
+print(f"there i saw {person}")
+print(f"we both did some {activity} which we enjoyed of course ")
+print(f"then we went to {place2}")
