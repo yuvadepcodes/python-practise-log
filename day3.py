5b3405d55e64cc4ab2d76ddaa560976e48a58e87
Yuvadep v <yuvadep.codes@gmail.com>
2025-10-15 22:12:55 +0530

    arithmatic operations and functions

---
+++ day3.py
@@ -0,0 +1,47 @@
+#arithmetic operators
+friend=5
+friend+=1
+print(friend)
+friend-=2
+print(friend)
+friend*=2
+print(friend)
+friend/=2
+print(friend)
+friend**=2
+print(friend)
+remainder=friend%3
+print(remainder)
+
+#functions
+x=3.4
+y=-4
+z=5
+result=round(x)
+print(result)
+
+is_y=abs(y)
+print(is_y)
+
+result=pow(4,3)
+print(result)
+
+result=max(x, y, z)
+print(result)
+
+result=min(x,y,z)
+print(result)
+
+import math
+print(math.pi)
+
+#square root 
+x=16.9
+result=math.sqrt(x)
+print(result)
+
+#floor and ceil
+result=math.floor(x)
+print(result)
+result =math.ceil(x)
+print(result)
