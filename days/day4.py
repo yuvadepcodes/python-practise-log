606151516884276ca768521fc4c8d33d28f2432f
Yuvadep v <yuvadep.codes@gmail.com>
2025-10-16 23:57:14 +0530

    if statement

---
+++ days/day4.py
@@ -0,0 +1,29 @@
+ # if statements
+age=int(input("Enter Your Age:" ))
+if age>=100:
+    print("Damn It You Are Too Old To Sign Up")
+elif age>=18:
+    print("Access Granted")
+elif age<=0:
+    print("Damn It You Havent Even Born Yet")
+else:
+    print(f"Your Age Is {age}, You Must Be Atleast 18 Years Old To Enter. ")
+
+
+
+
+
+for_sale=False
+if for_sale:
+      print("the item is on sale")
+else:
+     print("the item is not for sale")
+      
+
+
+online=input("Enter True Or False: ")
+if online==y:
+     print("Your Friend Is Online")
+else:
+     print("Your Frined Is Not Online")
+      
